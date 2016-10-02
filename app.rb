e152ef16e07723e5f39863841d270691114c3da8
Kensuke Nagae <kyanny@gmail.com>
2016-10-03 03:52:38 +0900

    Revert "Somehow `reply_message` doesn't work"
    This reverts commit ac931c52f7f5ff696511cf91676290df3fb82d2f.

--- app.rb
+++ app.rb
@@ -53,8 +53,7 @@ post '/callback' do
           type: 'text',
           text: event.message['text']
         }
-        # client.reply_message(event['replyToken'], message)
-        client.push_message(event['source']['userId'], message)
+        client.reply_message(event['replyToken'], message)
       end
     end
   }
