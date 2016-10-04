f7213078d7ccc86adf038cefcfc40b7e58c07c37
Kensuke Nagae <kyanny@gmail.com>
2016-10-04 13:34:36 +0900

    debug

--- app.rb
+++ app.rb
@@ -28,17 +28,19 @@ class HTTPProxyClient
 end
 
 def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.httpclient = HTTPProxyClient.new
+  }
   p ['@client = ', @client,
      @client.httpclient.instance_variable_get('@proxy_host'),
      @client.httpclient.instance_variable_get('@proxy_port'),
      @client.httpclient.instance_variable_get('@proxy_user'),
      @client.httpclient.instance_variable_get('@proxy_pass'),
     ]
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    config.httpclient = HTTPProxyClient.new
-  }
+
+  @client
 end
 
 post '/callback' do
