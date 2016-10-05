cae81c2ce5409b3dd3e5a3efd9cf757cf37e4646
Kensuke Nagae <kyanny@gmail.com>
2016-10-06 02:13:58 +0900

    back to original

--- app.rb
+++ app.rb
@@ -17,12 +17,14 @@ module Line
   end
 end
 
-post '/callback' do
-  client = Line::Bot::Client.new { |config|
+def client
+  @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
+end
 
+post '/callback' do
   body = request.body.read
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
