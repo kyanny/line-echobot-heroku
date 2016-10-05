1d4c8d5053a4138be425a9ec92f002a70b7a666b
Kensuke Nagae <kyanny@gmail.com>
2016-10-06 01:39:48 +0900

    RestClient

--- app.rb
+++ app.rb
@@ -1,44 +1,60 @@
 require 'sinatra'   # gem 'sinatra'
 require 'line/bot'  # gem 'line-bot-api'
+require 'rest-client'
 
-class HTTPProxyClient
-
-  def http(uri)
-    require 'uri'
-    proxy_uri = URI(ENV["FIXIE_URL"])
-    p proxy_uri
-    p [proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password]
-    http = Net::HTTP.new(uri.host, uri.port, proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password)
-    p [
-      http.proxy_address,
-      http.proxy_port,
-      http.proxy_user,
-      http.proxy_pass,
-    ]
-    if uri.scheme == "https"
-      http.use_ssl = true
-    end
-
-    http
-  end
-
+class MyRestClient
   def get(url, header = {})
-    uri = URI(url)
-    http(uri).get(uri.request_uri, header)
+    RestClient.get(url, header: header)
   end
 
   def post(url, payload, header = {})
-    uri = URI(url)
-    http(uri).post(uri.request_uri, payload, header)
+    RestClient.post(url, payload, header: header)
   end
+end
 
+# class HTTPProxyClient
+
+#   def http(uri)
+#     require 'uri'
+#     proxy_uri = URI(ENV["FIXIE_URL"])
+#     p proxy_uri
+#     p [proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password]
+#     http = Net::HTTP.new(uri.host, uri.port, proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password)
+#     p [
+#       http.proxy_address,
+#       http.proxy_port,
+#       http.proxy_user,
+#       http.proxy_pass,
+#     ]
+#     if uri.scheme == "https"
+#       http.use_ssl = true
+#     end
+
+#     http
+#   end
+
+#   def get(url, header = {})
+#     uri = URI(url)
+#     http(uri).get(uri.request_uri, header)
+#   end
+
+#   def post(url, payload, header = {})
+#     uri = URI(url)
+#     http(uri).post(uri.request_uri, payload, header)
+#   end
+
+# end
+
+configure do
+  RestClient.proxy = ENV['FIXIE_URL']
 end
 
 post '/callback' do
   client = Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    config.httpclient = HTTPProxyClient.new
+    # config.httpclient = HTTPProxyClient.new
+    config.httpclient = MyRestClient.new
   }
 
   body = request.body.read
